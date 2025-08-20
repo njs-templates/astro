163ed9115a7ba2028dfacd159525e4fe9576a5c5
njs-guy <shockeynj@gmail.com>
2025-08-20 19:02:55 -0400

    Fixed a build error

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,15 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+	// If you want to use sharp with PNPM:
+	// https://docs.astro.build/en/guides/images/#default-image-service
+	image: {
+		service: passthroughImageService(),
+	},
 	integrations: [sitemap()],
 	vite: {
 		plugins: [tailwindcss()],
